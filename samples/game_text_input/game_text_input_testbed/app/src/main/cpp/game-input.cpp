c3dec3e7de1970e44e9ab3e7914f6ca1f292eab6
Oleh Kuznetsov <okuznetsov@google.com>
2024-09-12 14:10:45 +0100

    Remove IdentWithd: 4 from .clang-format Reformat input related sources
    Test: not required
    Bug: n/a
    
    Change-Id: I389e6c3015b67fa0241d31e2470f1e950b47eb3d

--- samples/game_text_input/game_text_input_testbed/app/src/main/cpp/game-input.cpp
+++ samples/game_text_input/game_text_input_testbed/app/src/main/cpp/game-input.cpp
@@ -24,8 +24,8 @@
 // Import the Game Text Input interfae:
 #include <game-text-input/gametextinput.h>
 
-// Also include the source of Game Text Input in one of your C++ file, so that the
-// library is compiled as part of your build:
+// Also include the source of Game Text Input in one of your C++ file, so that
+// the library is compiled as part of your build:
 #include <game-text-input/gametextinput.cpp>
 
 static GameTextInput *gameTextInput = nullptr;
@@ -42,12 +42,14 @@ static void onEvent(void *context, const GameTextInputState *state) {
 }
 
 extern "C" JNIEXPORT void JNICALL
-Java_com_gametextinput_testbed_MainActivity_onCreated(JNIEnv *env, jobject thiz) {
+Java_com_gametextinput_testbed_MainActivity_onCreated(JNIEnv *env,
+                                                      jobject thiz) {
   ALOGD("Calling GameTextInput_init...");
   gameTextInput = GameTextInput_init(env, 0);
   activity = env->NewGlobalRef(thiz);
   ALOGD("activity is %p...", thiz);
-  jclass activityClass = env->FindClass("com/gametextinput/testbed/MainActivity");
+  jclass activityClass =
+      env->FindClass("com/gametextinput/testbed/MainActivity");
 
   setDisplayedText = env->GetMethodID(activityClass, "setDisplayedText",
                                       "(Ljava/lang/String;)V");
@@ -82,26 +84,36 @@ Java_com_gametextinput_testbed_MainActivity_hideIme(JNIEnv *env, jobject thiz) {
 }
 
 extern "C" JNIEXPORT void JNICALL
-Java_com_gametextinput_testbed_MainActivity_restartInput(JNIEnv *env, jobject thiz) {
+Java_com_gametextinput_testbed_MainActivity_restartInput(JNIEnv *env,
+                                                         jobject thiz) {
   GameTextInput_restartInput(gameTextInput);
 }
 
 extern "C" JNIEXPORT void JNICALL
 Java_com_gametextinput_testbed_MainActivity_sendSelectionToStart(JNIEnv *env,
-                                                             jobject thiz) {
-  GameTextInput_getState(gameTextInput, [](void* context, const GameTextInputState* state) {
-    GameTextInputState state_copy = *state;
-    state_copy.selection = {0, 0};
-    GameTextInput_setState(static_cast<GameTextInput*>(context), &state_copy);
-    }, (void*)gameTextInput);
+                                                                 jobject thiz) {
+  GameTextInput_getState(
+      gameTextInput,
+      [](void *context, const GameTextInputState *state) {
+        GameTextInputState state_copy = *state;
+        state_copy.selection = {0, 0};
+        GameTextInput_setState(static_cast<GameTextInput *>(context),
+                               &state_copy);
+      },
+      (void *)gameTextInput);
 }
 
 extern "C" JNIEXPORT void JNICALL
 Java_com_gametextinput_testbed_MainActivity_sendSelectionToEnd(JNIEnv *env,
-                                                           jobject thiz) {
-  GameTextInput_getState(gameTextInput, [](void* context, const GameTextInputState* state) {
-    GameTextInputState state_copy = *state;
-    state_copy.selection = {(int)state->text_length, (int)state->text_length};
-    GameTextInput_setState(static_cast<GameTextInput*>(context), &state_copy);
-  }, (void*)gameTextInput);
+                                                               jobject thiz) {
+  GameTextInput_getState(
+      gameTextInput,
+      [](void *context, const GameTextInputState *state) {
+        GameTextInputState state_copy = *state;
+        state_copy.selection = {(int)state->text_length,
+                                (int)state->text_length};
+        GameTextInput_setState(static_cast<GameTextInput *>(context),
+                               &state_copy);
+      },
+      (void *)gameTextInput);
 }
